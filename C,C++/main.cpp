a2ee71bc676b2e8d08506c27bea0a41e12891575
GauravBurande <burandegaurav8899@gmail.com>
2022-07-31 18:50:33 +0600

    intial commit C,C++

---
+++ C,C++/main.cpp
@@ -0,0 +1,96 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    // cout<<"Hello World"<<endl;
+
+    // int a, b;
+    // cout<<"enter the first number"<<endl;
+    // cin>>a;
+
+    // cout<<"enter the second number"<<endl;
+    // cin>>b;
+
+    // cout<<"a + b is  "<<a + b<<endl;
+    // cout<<"a - b is  "<<a - b<<endl;
+    // cout<<"a * b is  "<<a * b<<endl;
+    // cout<<"a / b is  "<<(float) a / b<<endl;
+
+    // int age;
+
+    // cout<<"enter your age"<<endl;
+    // cin>>age;
+    // if(age>121 || age<1){
+    // cout<<"invalid response";
+    // }
+
+    // else if(age>=18){
+    // cout<<"you can vote";
+    // }
+
+    // else{
+    // cout<<"you're still a child";
+    // }
+
+    // int age;
+    // cout << "enter your age" << endl;
+    // cin >> age;
+
+    // switch (age)
+    // {
+    // case 12:
+        // cout << "you're 12 years old";
+        // break;
+
+    // case 18:
+        // cout << "you're 18 years old";
+        // break;
+
+    // default:
+        // cout << "you're neither 12 nor 18 years old";
+        // break;
+    // }
+
+    // for (int i = 0; i<12; i++){
+        // cout<<"the number is "<<i<<endl;
+    // }
+
+    // int arr[3] = {1,2,3};
+    // cout<<arr[0];
+
+    // int arr2d[2][3] = {
+        // {1,2,3},
+        // {4,5,6}
+    // };
+
+    // for (int i = 0; i < 2; i++)
+    // {
+        // for (int j = 0; j < 3; j++)
+        // {
+            // cout<<"the value at ("<<i<<","<<j<<") is "<<arr2d[i][j]<<endl;
+        // }
+        // 
+    // }
+    // 
+
+    // int a = 343;
+    // cout<<(float) a/34;
+
+    // string name = "garry";
+    // cout<<"the name is "<<name<<endl;
+    // cout<<"the length of the name is "<<name.length()<<endl;
+    // cout<<"the name is "<<name.substr(0,344)<<endl;
+    // cout<<"the name is "<<name.substr(2,3)<<endl;
+
+    // int a = 34;
+    // int* ptra;
+    // ptra = &a;
+    // cout<<"the address of a is "<<ptra<<endl;
+    // cout<<"The value of a is "<<*ptra;
+
+    
+
+    return 0;
+}
