151d512b29aad46458cc6dec86ccf96b1c000ecd
GauravBurande <burandegaurav8899@gmail.com>
2022-08-08 14:35:11 +0600

    added stack_using_array.c

---
+++ Stack/stack_operations.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct stack
+{
+    int size;
+    int top;
+    int *arr;
+};
+
+int isFull(struct stack *ptr)
+{
+    
+}
+
+int main()
+{
+    struct stack *sp = (struct stack *)malloc(sizeof(struct stack));
+    sp->size = 10;
+    sp->top = -1;
+    sp->arr = (int *)malloc(sp->size * sizeof(int));
+    printf("stack has been created successfully!\n");
+
+
+
+    return 0;
+}
